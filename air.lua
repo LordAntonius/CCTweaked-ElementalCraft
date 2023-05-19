f5eb077b9375a327d18f810de794e2b5cebc3828
BuildTools <unconfigured@null.spigotmc.org>
2023-05-19 17:00:27 +0200

    test air crystal

--- air.lua
+++ air.lua
@@ -2,10 +2,11 @@
 local MESide = "left"
 local binderSide = "right"
 
--- Variableq
+-- Variables
 local ME = peripheral.wrap(MESide)
 
-local inert_crystal_name = "elementalcraft:inert_crystal"
+--local inert_crystal_name = "elementalcraft:inert_crystal"
+local air_crystal_recipe = {"elementalcraft:inert_crystal"}
 
 local function MECraft (name, number)
     local item = {
@@ -15,7 +16,7 @@ local function MECraft (name, number)
     if ME.isItemCraftable(item) then
         ME.craftItem(item)
         repeat sleep(0.5)
-        until ME.isItemCrafting({name = name})
+        until not ME.isItemCrafting({name = name})
     end
 end
 
@@ -23,7 +24,6 @@ local function MEAutocraft (name, number)
     local item = {
         name = name
     }
-
     curItem = ME.getItem(item)
     local diff = number - curItem.amount
 
@@ -32,7 +32,23 @@ local function MEAutocraft (name, number)
     end
 end
 
-while true do
-    MEAutocraft("minecraft:gold_ingot", 64)
-    sleep(1)
+local function elementalcraftCraftSequence(t, itemName)
+    for k,v in t do
+        MEAutocraft(v,1)
+        if ME.getItem({name = v}).amount < 1 then
+            -- Not enough items
+            return false
+        end
+        ME.exportItemToPeripheral({name=v, amount = 1}, binderSide)
+        repeat 
+            local n = ME.importItemFromPeripheral(itemName, binderSide)
+            sleep(0.5)
+        until n > 0
+    end
 end
+
+elementalcraftCraftSequence(air_crystal_recipe, "elementalcraft:aircrystal")
+--while true do
+    --MEAutocraft("minecraft:gold_ingot", 64)
+    sleep(1)
+--end
