5b6ab71d797247eb8e3b15f6a155911631c878d0
BuildTools <unconfigured@null.spigotmc.org>
2023-05-19 16:25:30 +0200

    test

--- air.lua
+++ air.lua
@@ -7,28 +7,30 @@ local ME = peripheral.wrap(MESide)
 
 local inert_crystal_name = "elementalcraft:inert_crystal"
 
-local function MEcraft (name, number) {
+local function MECraft (name, number)
     local item = {
         name = name,
         count = number
     }
     if ME.isItemCraftable(item) then
         ME.craftItem(item)
+        repeat sleep(0.5)
+        until ME.isItemCrafting({name = name})
     end
-}
+end
 
-local function MEAutocraft (name, number) {
+local function MEAutocraft (name, number)
     local item = {
         name = name
     }
 
     curItem = ME.getItem(item)
-    local diff = curItem.amount - number
+    local diff = number - curItem.amount
 
     if diff > 0 then
-        MEcraft(name, diff)
+        MECraft(name, diff)
     end
-}
+end
 
 while true do
     MEAutocraft("minecraft:gold_ingot", 64)
